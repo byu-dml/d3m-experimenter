96fb97837e5fdcaeca0646863fe744a54ea6d791
orionw <wellerorion@gmail.com>
2019-03-04 14:26:12 -0700

    remove extra inputs

--- experimenter/experimenter.py
+++ experimenter/experimenter.py
@@ -147,8 +147,6 @@ class Experimenter:
 
         # Creating Pipeline
         pipeline_description = Pipeline(context=Context.TESTING)
-        pipeline_description.add_input(name='inputs')
-
 
         step_counter = self._add_initial_steps(pipeline_description, step_counter)
 
