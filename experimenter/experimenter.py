b0e32f3547ece53ae527cb50a252fa4ea3a9c65a
Brandon Schoenfeld <bjschoenfeld@gmail.com>
2019-03-11 16:50:22 -0600

    get possible problems cleanup with utils

--- experimenter/experimenter.py
+++ experimenter/experimenter.py
@@ -17,6 +17,8 @@ import json
 import sys
 from .database_communication import PipelineDB
 
+from experimenter import utils
+
 logger = logging.getLogger(__name__)
 
 
@@ -183,13 +185,12 @@ class Experimenter:
     def get_possible_problems(self):
         problems_list = {"classification": [], "regression": []}
         for problem_directory in self.problem_directories:
-            datasets_suffix = problem_directory
-            datasets_dir = os.path.join(self.datasets_dir, datasets_suffix)
-            for problem_name in os.listdir(datasets_dir):
-                problem_description_path = os.path.join(datasets_dir, problem_name, problem_name + "_problem", 'problemDoc.json')
-                problem_type = self.get_problem_type(problem_name, [problem_description_path])
+            datasets_dir = os.path.join(self.datasets_dir, problem_directory)
+            for dataset_name in os.listdir(datasets_dir):
+                problem_description_path = utils.get_problem_path(dataset_name, datasets_dir)
+                problem_type = self.get_problem_type(dataset_name, [problem_description_path])
                 if problem_type in problems_list:
-                    problems_list[problem_type].append(os.path.join(datasets_dir, problem_name))
+                    problems_list[problem_type].append(os.path.join(datasets_dir, dataset_name))
         return problems_list
 
     def generate_pipelines(self, preprocessors: List[str], models: dict):
