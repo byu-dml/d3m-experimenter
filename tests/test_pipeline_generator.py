ab1631893a5178baa429eef9a90bdbd7c25419ca
Brandon Schoenfeld <bjschoenfeld@gmail.com>
2019-03-13 12:12:43 -0600

    updated tests

--- tests/test_pipeline_generator.py
+++ tests/test_pipeline_generator.py
@@ -52,20 +52,20 @@ class PipelineGenerationTestCase(unittest.TestCase):
     
     def test_basic_pipeline_structure(self):
 
-        num_pipeline_steps = 6  # Denormalize/DatasetToDataFrame/ColumnParser/SKImputer/SKGaussianNB/Construct Predictions
-        pipeline_step_zero = 'd3m.primitives.datasets.Denormalize'
-        pipeline_step_fifth = 'd3m.primitives.data.ConstructPredictions'
+        num_pipeline_steps = 5  # DatasetToDataFrame/ColumnParser/SKImputer/SKGaussianNB/Construct Predictions
+        pipeline_step_zero = 'd3m.primitives.data_transformation.dataset_to_dataframe.Common'
+        pipeline_step_four = 'd3m.primitives.data_transformation.construct_predictions.DataFrameCommon'
 
         print("Testing that the pipelines are what they should be...")
         # should only make one pipeline with no preprocessor
         self.assertEqual(
-            1, len(self.experimenter_driver.generated_pipelines),
+            1, len(self.experimenter_driver.generated_pipelines['classification']),
             'Expected 1 pipeline to be generated, but got {}'.format(
                 len(self.experimenter_driver.generated_pipelines)
             )
         )
-        generated_pipelines = self.experimenter_driver.generated_pipelines[0].to_json_structure()
+        generated_pipelines = self.experimenter_driver.generated_pipelines['classification'][0].to_json_structure()
         # make sure there are the normal number of steps in the pipeline
         self.assertEqual(len(generated_pipelines['steps']), num_pipeline_steps)
         self.assertEqual(generated_pipelines['steps'][0]['primitive']['python_path'], pipeline_step_zero)
-        self.assertEqual(generated_pipelines['steps'][5]['primitive']['python_path'], pipeline_step_fifth)
+        self.assertEqual(generated_pipelines['steps'][4]['primitive']['python_path'], pipeline_step_four)
