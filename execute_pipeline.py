42cd797ce7c40fd942db21929cf9994caefe3037
orionw <wellerorion@gmail.com>
2019-03-13 15:24:02 -0600

    working pipeline system

--- execute_pipeline.py
+++ execute_pipeline.py
@@ -1,7 +1,24 @@
+"""
+FILE INFORMATION:
+This file needs to be a stand alone file so that it can be imported and used by the experimenter_driver.py.  This is
+because RQ only accepts a function that is imported and not defined in __main__.  These functions are what is needed
+to execute a pipeline on a problem and can be used by an individual machine, or used in a RQ job queue.
+"""
+
 from experimenter.database_communication import PipelineDB
 from experimenter.pipeline.run_pipeline import RunPipeline
 
 
+"""
+The main function to execute a pipeline.  Called in `experimenter_driver.py`  This function will check if the 
+pipeline and dataset has been executed before, run the pipeline, and record the results
+
+:param pipe: the pipeline object that will be executed
+:param problem: the path to the problemDoc of the particular dataset
+:param datasets_dir: a string containing the main directory of datasets
+:param volumes_dir: a string containing the path to the volumes directory
+:param pipeline_path: a path to the pipeline file to be executed
+"""
 def execute_pipeline_on_problem(pipe, problem, datasets_dir, volumes_dir, pipeline_path=""):
     # Attempt to run the pipeline
     print("\n On problem {}".format(problem))
@@ -11,7 +28,7 @@ def execute_pipeline_on_problem(pipe, problem, datasets_dir, volumes_dir, pipeli
         print("Already ran. Skipping")
         return
     run_pipeline = RunPipeline(datasets_dir, volumes_dir, pipeline_path, problem)
-    results = run_pipeline.run(problem, pipe)[0]
+    results = run_pipeline.run(pipeline=pipe)[0]
     score = results[0]
     # fit_pipeline_run = results[1]
     produce_pipeline_run = results[2]
@@ -19,31 +36,53 @@ def execute_pipeline_on_problem(pipe, problem, datasets_dir, volumes_dir, pipeli
                                             pipe.to_json_structure(), score, problem, mongo_db)
 
 
+"""
+Called after a successful pipeline run.  It will output the results to the console and write it to the database
+
+:param pipeline_run: the pipeline run object that will be recorded
+:param pipeline: the pipeline that was run
+:param score: the results from the execution of the pipeline
+:param problem: the problem that was used
+:param mongo_db: a connection to the MongoDB database
+"""
 def handle_successful_pipeline_run(pipeline_run, pipeline, score, problem, mongo_db):
     if score["value"][0] == 0:
         # F-SCORE was calculated wrong - quit and don't keep this run
         return
     problem_name = problem.split("/")[-1]
-    print_successful_pipeline_run(pipeline_run, pipeline, score)
-    write_to_mongo(mongo_db, pipeline_run, pipeline, problem_name)
+    print_successful_pipeline_run(pipeline, score)
+    write_to_mongo(mongo_db, pipeline_run)
 
+
+"""
+A simple function to print the pipeline and problem, for debugging
+
+:param pipeline: the pipeline that was executed
+:param problem: the dataset/problem that was used
+"""
 def print_pipeline_and_problem(pipeline, problem):
     print("Pipeline:")
     print(get_list_vertically(primitive_list_from_pipeline_object(pipeline)))
     print("on problem {} \n\n".format(problem))
 
-def get_primitive_combo_string(pipeline):
-    prim_string = ''
-    for p in pipeline['steps']:
-        prim_string += p['primitive']['id']
-    return prim_string
 
+"""
+A function to write a pipeline_run document to a database.  A wrapper for the function in database_communication.py
 
-def write_to_mongo(mongo_db, pipeline_run, pipeline, problem_name):
+:param mongo_db: the database connection
+:param pipeline_run: the json object to be written to the database
+"""
+def write_to_mongo(mongo_db, pipeline_run):
     mongo_db.add_to_pipeline_runs_mongo(pipeline_run)
 
 
-def print_successful_pipeline_run(pipeline_run, pipeline, score=None):
+"""
+A helper function for printing a succesful run
+
+:param pipeline: the pipeline that we will print
+:param score: the results of the metric used in training
+"""
+def print_successful_pipeline_run(pipeline, score=None):
     primitive_list = primitive_list_from_pipeline_json(pipeline)
     print("Ran pipeline:\n")
     print(get_list_vertically(primitive_list))
@@ -51,6 +90,11 @@ def print_successful_pipeline_run(pipeline_run, pipeline, score=None):
         print("With a {} of {}".format(score["metric"][0], score["value"][0]))
 
 
+"""
+A helper function to return all the primitives used in a pipeline
+
+:param pipeline: a pipeline object
+"""
 def primitive_list_from_pipeline_object( pipeline):
     primitives = []
     for p in pipeline.steps:
@@ -58,6 +102,11 @@ def primitive_list_from_pipeline_object( pipeline):
     return primitives
 
 
+"""
+A helper function to return all the primitives used in a pipeline
+
+:param pipeline_json a pipeline object in JSON form
+"""
 def primitive_list_from_pipeline_json( pipeline_json):
     primitives = []
     for step in pipeline_json['steps']:
@@ -65,5 +114,8 @@ def primitive_list_from_pipeline_json( pipeline_json):
     return primitives
 
 
+"""
+A helper function to join a list vertically.  Used for debugging printing.
+"""
 def get_list_vertically( list):
     return '\n'.join(list)
--- tests/test_pipeline_run.py
+++ tests/test_pipeline_run.py
@@ -1,30 +1,52 @@
 import argparse
 import unittest
-
 import yaml
-
 from d3m.runtime import handler, configure_parser
 from d3m.metadata import pipeline as pipeline_module
 from experimenter.pipeline.run_pipeline import RunPipeline
 
 
+class TestExecutingPipelines(unittest.TestCase):
 
-class PipelineGenerationTestCase(unittest.TestCase):
-
-    # @classmethod
-    # def setUpClass(cls):
-    #     cls.datasets_dir = sys.argv[1]  # TODO: move to .env
-    #     cls.seed_problem_directory = ['/seed_datasets_current/']
-
-    # @classmethod
     def setUp(self):
         self.datasets_dir = "/datasets"
         self.volumes_dir = "/volumes"
         self.pipeline_path = 'tests/testing_pipelines/test_bagging_classification.json'
 
-    def test_run_mice_protein_problem(self):
-        problem_path = "/datasets/seed_datasets_current/4550_MiceProtein/"
+    def test_d3m_runtime_works(self):
+        problem_path = "/datasets/seed_datasets_current/185_baseball"
+        problem_name = problem_path.split("/")[-1]
+        self.run_d3m(problem_name)
 
+    def test_experimenter_run_works_from_file(self):
+        problem_path = "/datasets/seed_datasets_current/185_baseball"
+        self.run_experimenter_from_file(problem_path)
+
+    def test_experimenter_run_works_from_pipeline(self):
+        problem_path = "/datasets/seed_datasets_current/185_baseball"
+        self.run_experimenter_from_pipeline(problem_path)
+
+    def test_systems_output_equal(self):
+        problem_path = "/datasets/seed_datasets_current/185_baseball"
+        problem_name = problem_path.split("/")[-1]
+
+        # run both systems
+        experimenter_score = self.run_experimenter_from_file(problem_path)
+        self.run_d3m(problem_name)
+
+        # get results from d3m output file
+        with open('test_results.yml', 'r') as file:
+            # grab the second document, containing the scores
+            d3m_score = list(yaml.load_all(file))
+            # get the F1-Macro from the first document
+            d3m_score = d3m_score[1]["run"]["results"]["scores"][0]["value"]
+
+        # compare systems
+        self.assertEqual(d3m_score, experimenter_score,
+                         "Our results from the experimenter were {} while D3M's results were {}".
+                         format(d3m_score, experimenter_score))
+
+    def run_d3m(self, problem_name):
         # Check that D3M is working and get results
         parser = argparse.ArgumentParser(description="Run D3M pipelines with default hyper-parameters.")
         configure_parser(parser)
@@ -33,34 +55,33 @@ class PipelineGenerationTestCase(unittest.TestCase):
                      '-n', 'tests/testing_pipelines/scoring.yml',
                      '-d', 'tests/testing_pipelines/fixed-split-tabular-split.yml',
                      '--data-split-file',
-                     '/datasets/seed_datasets_current/4550_MiceProtein/4550_MiceProtein_problem/dataSplits.csv',
-                     '-r', '/datasets/seed_datasets_current/4550_MiceProtein/4550_MiceProtein_problem/problemDoc.json',
-                     '-i', '/datasets/seed_datasets_current/4550_MiceProtein/4550_MiceProtein_dataset/datasetDoc.json',
+                     '/datasets/seed_datasets_current/{}/{}_problem/dataSplits.csv'.
+                         format(problem_name, problem_name),
+                     '-r', '/datasets/seed_datasets_current/{}/{}_problem/problemDoc.json'.
+                         format(problem_name, problem_name),
+                     '-i', '/datasets/seed_datasets_current/{}/{}_dataset/datasetDoc.json'.
+                         format(problem_name, problem_name),
                      '-O', 'test_results.yml']
 
         arguments = parser.parse_args(args=test_args)
         handler(arguments, parser)
-        quit(0)
-        import pdb; pdb.set_trace()
-        # get results form output file
-        with open('test_results.yml', 'r') as file:
-            # grab the second document, containing the scores
-            d3m_results = list(yaml.load_all(file))
-            # get the F1-Macro
-            d3m_results = d3m_results["run"]["results"]["predictions"]["scores"]["value"]
 
-        # Check our system
+    def run_experimenter_from_file(self, problem_path):
         # load pipeline
         with open(self.pipeline_path, 'r') as file:
             pipeline_to_run = pipeline_module.Pipeline.from_json(string_or_file=file)
 
         # run our system
         run_pipeline = RunPipeline(self.datasets_dir, self.volumes_dir, self.pipeline_path, problem_path)
-        results_test = run_pipeline.run(problem_path, pipeline_to_run)[0]
-        score = results_test[0]
+        results_test = run_pipeline.run(pipeline=pipeline_to_run)[0]
+        # the value of score is in the first document in the first index
+        score = results_test[0]["value"][0]
+        return score
 
-        # compare systems
-        self.assertEqual(d3m_results, score, "Our results from the experimenter were {} while D3M's results were {}".
-                         format(d3m_results, score))
-
-
+    def run_experimenter_from_pipeline(self, problem_path):
+        # run our system
+        run_pipeline = RunPipeline(self.datasets_dir, self.volumes_dir, self.pipeline_path, problem_path)
+        results_test = run_pipeline.run()[0]
+        # the value of score is in the first document in the first index
+        score = results_test[0]["value"][0]
+        return score
