99d20c84a4689b64694b9d39282fcf88d7de9645
orionw <wellerorion@gmail.com>
2019-08-07 07:28:43 -0600

    try to fail

--- run_tests.py
+++ run_tests.py
@@ -6,6 +6,6 @@ import unittest
 runner = unittest.TextTestRunner(verbosity=1)
 
 tests = unittest.TestLoader().discover('./test')
-
+raise Exception("Failed")
 if not runner.run(tests).wasSuccessful():
     sys.exit(1)
