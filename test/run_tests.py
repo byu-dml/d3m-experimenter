1934d4b1beb4af6606777943187514fabcbe1051
orionw <wellerorion@gmail.com>
2019-08-07 17:39:06 -0600

    moved run test file

--- test/run_tests.py
+++ test/run_tests.py
@@ -5,7 +5,7 @@ import unittest
 
 runner = unittest.TextTestRunner(verbosity=1)
 
-tests = unittest.TestLoader().discover('./test')
-raise Exception("Failed")
+tests = unittest.TestLoader().discover('.')
+
 if not runner.run(tests).wasSuccessful():
     sys.exit(1)
