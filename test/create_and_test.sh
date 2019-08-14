2388829acee21a517ead0bec504933d3aa12df41
Orion Weller <orionw@qualtrics.com>
2019-08-14 11:14:51 -0600

    removed dockerfile duplication

--- test/create_and_test.sh
+++ test/create_and_test.sh
@@ -1,3 +1,6 @@
+mkdir build
+cp ../requirements.txt build/requirements.txt
+cp ../Dockerfile build/test-Dockerfile
 docker-compose up -d --build
 sudo docker exec -it experimenter-test bash -c "ls"
 sudo docker exec -it experimenter-test bash -c "python3 run_tests.py"
