e91c312e7d8c9c872b21ed326d8b1e0ef1707034
orionw <wellerorion@gmail.com>
2019-08-15 08:13:52 -0600

    fixed docker env issue

--- test/create_and_test.sh
+++ test/create_and_test.sh
@@ -1,6 +1,8 @@
 mkdir build
 cp ../requirements.txt build/requirements.txt
 cp ../Dockerfile build/test-Dockerfile
+sed -i '$ d' build/test-Dockerfile  # removes the last line
+echo "RUN pip3 install -r build/requirements.txt" >> build/test-Dockerfile  # add in the new requirements file location
 docker-compose up -d --build
-sudo docker exec -it experimenter-test bash -c "ls"
 sudo docker exec -it experimenter-test bash -c "python3 run_tests.py"
+rm -rf build/
