fc2dedf5000d3dd4d25f3027e2df7497c8cda3bd
orionw <wellerorion@gmail.com>
2019-08-16 14:54:16 -0600

    try codecov

--- test/create_and_test.sh
+++ test/create_and_test.sh
@@ -4,5 +4,8 @@ cp ../Dockerfile build/test-Dockerfile
 sed -i '$ d' build/test-Dockerfile  # removes the last line
 echo "RUN pip3 install -r build/requirements.txt" >> build/test-Dockerfile  # add in the new requirements file location
 docker-compose up -d --build
-sudo docker exec -it experimenter-test bash -c "python3 run_tests.py"
+sudo docker exec -it experimenter-test bash -c "pip3 install coverage"
+sudo docker exec -it experimenter-test bash -c "pip3 install codecov"
+sudo docker exec -it experimenter-test bash -c "coverage run run_tests.py"
+sudo docker exec -it experimenter-test bash -c "codecov"
 rm -rf build/
