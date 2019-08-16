c96dc24df8ea737ac2ee5b803728555ed823f18b
orionw <wellerorion@gmail.com>
2019-08-16 16:30:35 -0600

    add more code cov config

--- test/create_and_test.sh
+++ test/create_and_test.sh
@@ -6,8 +6,6 @@ echo "RUN pip3 install -r build/requirements.txt" >> build/test-Dockerfile  # ad
 docker-compose up -d --build
 sudo docker exec -it experimenter-test bash -c "pip3 install coverage"
 sudo docker exec -it experimenter-test bash -c "pip3 install codecov"
-sudo docker exec -it experimenter-test bash -c "pip3 install codecov"
-sudo docker exec -it experimenter-test bash -c "CODECOV_TOKEN='66edf814-d6e1-40ae-b98a-dfea63a7e197'"
+sudo docker exec -it experimenter-test bash -c "export CODECOV_TOKEN='66edf814-d6e1-40ae-b98a-dfea63a7e197'"
 sudo docker exec -it experimenter-test bash -c "coverage run run_tests.py"
-sudo docker exec -it experimenter-test bash -c "codecov"
 rm -rf build/
