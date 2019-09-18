86537355f67f315f00e1be20add689cd316ab81a
orionw <wellerorion@gmail.com>
2019-09-18 11:08:19 -0600

    first codecov attempt

--- test/create_and_test.sh
+++ test/create_and_test.sh
@@ -3,9 +3,12 @@ cp ../requirements.txt build/requirements.txt
 cp ../Dockerfile build/test-Dockerfile
 sed -i '$ d' build/test-Dockerfile  # removes the last line
 echo "RUN pip3 install -r build/requirements.txt" >> build/test-Dockerfile  # add in the new requirements file location
+# request codecov to detect CI environment to pass through to docker
+export ci_env=`bash <(curl -s https://codecov.io/env)`
 docker-compose up -d --build
 sudo docker exec -it experimenter-test bash -c "pip3 install coverage"
 sudo docker exec -it experimenter-test bash -c "pip3 install codecov"
 sudo docker exec -it experimenter-test bash -c "export CODECOV_TOKEN='66edf814-d6e1-40ae-b98a-dfea63a7e197'"
 sudo docker exec -it experimenter-test bash -c "coverage run run_tests.py"
+ sudo docker exec -it experimenter-test bash -c "bash <(curl -s https://codecov.io/bash)"
 rm -rf build/
