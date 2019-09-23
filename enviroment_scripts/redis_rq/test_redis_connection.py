64436093374539e6c1b7c463c4f0777b5c84d14b
orionw <wellerorion@gmail.com>
2019-09-23 15:15:49 -0600

    missing script

---
+++ enviroment_scripts/redis_rq/test_redis_connection.py
@@ -0,0 +1,20 @@
+import redis
+import os
+
+try:
+    redis_host = os.environ['REDIS_HOST']
+    redis_port = int(os.environ['REDIS_PORT'])
+except Exception as E:
+    print("Exception: environment variables not set")
+    raise E
+
+try:
+    conn = redis.StrictRedis(
+        host=redis_host,
+        port=redis_port)
+    print(conn)
+    conn.ping()
+    print('Connected!')
+except Exception as ex:
+    print('Error:', ex)
+    raise Exception('Failed to connect, terminating.')
