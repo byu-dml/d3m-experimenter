9a67d399383d2425bf916d332c54bb83b22e80bc
Evan Peterson <evanpeterson17@gmail.com>
2019-11-19 15:09:58 -0700

    Dont install tests with package

--- setup.py
+++ setup.py
@@ -1,4 +1,4 @@
-from setuptools import setup
+from setuptools import setup, find_packages
 
 with open('requirements.txt', 'r') as f:
     install_requires = list()
@@ -8,14 +8,14 @@ with open('requirements.txt', 'r') as f:
             install_requires.append(re)
 
 setup(
-    name='d3mexperimenter',
+    name='d3m_experimenter',
     version='1.0',
     description='Package for building and running D3M experiments',
     author='BYU DML',
     # Just one author of many
     author_email='evanpeterson17@gmail.com',
     url='https://github.com/byu-dml/d3m-experimenter',
-    packages=['experimenter'],
+    packages=find_packages(include=['experimenter', 'experimenter.*']),
     python_requires='>=3.6',
     install_requires=install_requires,
 )
