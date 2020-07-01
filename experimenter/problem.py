e9cdabb173802bfaffeced5cf6dcaa51e0e9d00b
epeters3 <evanpeterson17@gmail.com>
2020-07-01 17:17:25 -0600

    Problem class supports subsets e.g. TEST, SCORE, etc.

--- experimenter/problem.py
+++ experimenter/problem.py
@@ -11,7 +11,11 @@ logger = logging.getLogger(__name__)
 
 
 class ProblemReference:
-    def __init__(self, name: str, directory: str, datasets_dir: str) -> None:
+    subsets = {"ALL", "TRAIN", "TEST", "SCORE"}
+
+    def __init__(
+        self, name: str, directory: str, datasets_dir: str, subset: str = "ALL"
+    ) -> None:
         """
         :param name: The name of the directory where this problem's files
             can be found.
@@ -20,25 +24,130 @@ class ProblemReference:
         :param datasets_dir: The root datasets directory which contains all the
             problem directories (problem directories may not be direct children
             of this).
+        :param subset: The subset of this problem/dataset to use.
         """
         # The name of the problem/dataset. Also the same as the root directory
         # the problem lives in.
         self.name = name
         # The path to the root directory of the problem and dataset.
         self.path = os.path.join(datasets_dir, directory, self.name)
-        self._load_and_set_attributes()
+
+        assert subset in self.subsets
+        self.subset = subset
+
+        self._load_problem_docs()
+        self._load_dataset_docs()
+
+    # Problem-related attributes and methods
+
+    def _load_problem_docs(self) -> None:
+        # We'll temporarilly change `self.subset` in order to load
+        # all the problem docs.
+        user_subset = self.subset
+
+        self._problem_docs = {}
+        for subset in self.subsets:
+            self.subset = subset
+            if os.path.isfile(self.problem_doc_path):
+                self._problem_docs[self.subset] = json.load(open(self.problem_doc_path))
+
+        self.subset = user_subset
+
+    @property
+    def problem_dir(self) -> str:
+        if self.subset == "ALL":
+            return os.path.join(self.path, f"{self.name}_problem")
+        elif self.subset in self.subsets:
+            return os.path.join(self.path, self.subset, f"problem_{self.subset}")
+        else:
+            raise ValueError(
+                f"Unsupported value for self.subset '{self.subset}'. "
+                f"Valid options are {self.subsets}"
+            )
 
     @property
     def data_splits_path(self) -> str:
-        return os.path.join(self.path, f"{self.name}_problem", "dataSplits.csv")
+        return os.path.join(self.problem_dir, "dataSplits.csv")
 
     @property
     def problem_doc_path(self) -> str:
-        return os.path.join(self.path, f"{self.name}_problem", "problemDoc.json")
+        return os.path.join(self.problem_dir, "problemDoc.json")
+
+    @property
+    def problem_doc(self) -> dict:
+        if self.subset not in self._problem_docs:
+            # This problem has no problem document for the
+            # `self.subset` subset.
+            return None
+        return self._problem_docs[self.subset]
+
+    @property
+    def task_keywords(self) -> list:
+        return self.problem_doc["about"]["taskKeywords"]
+
+    @property
+    def problem_type(self):
+        if "classification" in self.task_keywords:
+            return "classification"
+        elif "regression" in self.task_keywords:
+            return "regression"
+        else:
+            return None
+
+    def to_d3m_problem(self) -> D3MProblem:
+        return D3MProblem.load(f"file://{self.problem_doc_path}")
+
+    # Dataset-related attributes and methods
+
+    def _load_dataset_docs(self) -> None:
+        # We'll temporarilly change `self.subset` in order to load
+        # all the dataset docs.
+        user_subset = self.subset
+
+        self._dataset_docs = {}
+        for subset in self.subsets:
+            self.subset = subset
+            if os.path.isfile(self.dataset_doc_path):
+                self._dataset_docs[self.subset] = json.load(open(self.dataset_doc_path))
+
+        self.subset = user_subset
+
+    @property
+    def dataset_dir(self) -> str:
+        if self.subset == "ALL":
+            return os.path.join(self.path, f"{self.name}_dataset")
+        elif self.subset in self.subsets:
+            return os.path.join(self.path, self.subset, f"dataset_{self.subset}")
+        else:
+            raise ValueError(
+                f"Unsupported value for self.subset '{self.subset}'. "
+                f"Valid options are {self.subsets}"
+            )
 
     @property
     def dataset_doc_path(self) -> str:
-        return os.path.join(self.path, f"{self.name}_dataset", "datasetDoc.json")
+        return os.path.join(self.dataset_dir, "datasetDoc.json")
+
+    @property
+    def dataset_doc(self) -> dict:
+        if self.subset not in self._dataset_docs:
+            # This problem has no dataset document for the
+            # `self.subset` subset.
+            return None
+        return self._dataset_docs[self.subset]
+
+    @property
+    def dataset_id(self) -> str:
+        return self.dataset_doc["about"]["datasetID"]
+
+    @property
+    def dataset_digest(self) -> str:
+        return self.dataset_doc["about"]["digest"]
+
+    def to_d3m_dataset(self) -> Dataset:
+        return Dataset.load(f"file://{self.dataset_doc_path}")
+
+    # Other attributes and methods
 
     @property
     def is_blacklisted(self) -> bool:
@@ -48,38 +157,3 @@ class ProblemReference:
         """
         normalized_name = self.name.replace("_MIN_METADATA", "")
         return normalized_name in PROBLEM_BLACKLIST
-
-    def get_dataset_id(self) -> str:
-        return json.load(open(self.dataset_doc_path))["about"]["datasetID"]
-
-    def to_d3m_problem(self) -> D3MProblem:
-        return D3MProblem.load(f"file://{self.problem_doc_path}")
-
-    def to_d3m_dataset(self) -> Dataset:
-        return Dataset.load(f"file://{self.dataset_doc_path}")
-
-    def _load_and_set_attributes(self):
-        """
-        Gathers various attributes about the problem and its dataset
-        using its problem document and dataset document.
-        """
-        # Gather data about the problem.
-        with open(self.problem_doc_path, "r") as f:
-            problem_doc = json.load(f)
-
-        self.task_keywords = problem_doc["about"]["taskKeywords"]
-        if "classification" in self.task_keywords:
-            self.problem_type = "classification"
-        elif "regression" in self.task_keywords:
-            self.problem_type = "regression"
-        else:
-            self.problem_type = None
-
-        self.problem_id = problem_doc["about"]["problemID"]
-
-        # Gather data about the problem's dataset.
-        with open(self.dataset_doc_path, "r") as f:
-            dataset_doc = json.load(f)
-
-        self.dataset_id = dataset_doc["about"]["datasetID"]
-        self.dataset_digest = dataset_doc["about"]["digest"]
