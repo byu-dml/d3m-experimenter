1cde8456f159c37716635e69951cf2fa0721daee
Brandon Schoenfeld <bjschoenfeld@gmail.com>
2021-01-14 22:35:45 -0700

    started job generator cli

--- experimenter/cli.py
+++ experimenter/cli.py
@@ -1,7 +1,7 @@
 import argparse
 import typing
 
-from experimenter import queue
+from experimenter import exceptions, queue
 
 
 def main(argv: typing.Sequence) -> None:
@@ -15,8 +15,17 @@ def configure_parser(parser: argparse.ArgumentParser) -> None:
     subparsers = parser.add_subparsers(dest='experimenter_command')
     subparsers.required = True
 
-    queue_parser = subparsers.add_parser('queue')
+    queue_parser = subparsers.add_parser(
+        'queue',
+        description='control the job queue',
+    )
     configure_queue_parser(queue_parser)
+
+    generator_parser = subparsers.add_parser(
+        'generator',
+        description='generates new pipelines and queues them to run on available datasets',
+    )
+    configure_generator_parser(generator_parser)
 
 
 def handler(arguments: argparse.Namespace, parser: argparse.ArgumentParser) -> None:
@@ -25,6 +34,8 @@ def handler(arguments: argparse.Namespace, parser: argparse.ArgumentParser) -> N
 
     if experimenter_command == 'queue':
         queue_handler(arguments, subparser)
+    elif experimenter_command == 'generator':
+        generator_handler(arguments, subparser)
     else:
         raise exceptions.InvalidStateError('Unknown experimenter command: {}'.format(experimenter_command))
 
@@ -53,3 +64,66 @@ def queue_handler(arguments: argparse.Namespace, parser: argparse.ArgumentParser
         queue.status()
     else:
         raise exceptions.InvalidStateError('Unknown queue command: {}'.format(queue_command))
+
+
+def configure_generator_parser(parser: argparse.ArgumentParser) -> None:
+    parser.add_argument('--n-max-jobs', type=int, default=None, action='store')  # TODO:, description='the max number of jobs to generate')
+
+    subparsers = parser.add_subparsers(dest='generator_command')
+    subparsers.required = True
+
+    search_subparser = subparsers.add_parser(
+        'search',
+        description='searches for new pipelines not found in the metalearning database',
+    )
+    configure_search_parser(search_subparser)
+
+    edit_subparser = subparsers.add_parser(
+        'modify',
+        description='modifies existing pipelines in the metalearning database',
+    )
+    configure_edit_parser(edit_subparser)
+
+    update_subparser = subparsers.add_parser(
+        'update',
+        description='updates existing pipeline runs in the metalearning database to use the current versions of datasets and primitives',
+    )
+    configure_update_parser(update_subparser)
+
+
+def generator_handler(arguments: argparse.Namespace, parser: argparse.ArgumentParser) -> None:
+    generator_command = arguments.generator_command
+    subparser = parser._subparsers._group_actions[0].choices[generator_command]  # type: ignore
+
+    if generator_command == 'search':
+        search_handler(arguments, subparser)
+    elif generator_command == 'modify':
+        modify_handler(arguments, subparser)
+    elif generator_command == 'update':
+        update_handler(arguments, subparser)
+    else:
+        raise exceptions.InvalidStateError('Unknown queue command: {}'.format(generator_command))
+
+
+def configure_search_parser(parser: argparse.ArgumentParser) -> None:
+    pass
+
+
+def search_handler(arguments: argparse.Namespace, parser: argparse.ArgumentParser) -> None:
+    raise exceptions.NotImplementedError()
+
+
+def configure_edit_parser(parser: argparse.ArgumentParser) -> None:
+    pass
+
+
+def modify_handler(arguments: argparse.Namespace, parser: argparse.ArgumentParser) -> None:
+    raise exceptions.NotImplementedError()
+
+
+def configure_update_parser(parser: argparse.ArgumentParser) -> None:
+    pass
+
+
+def update_handler(arguments: argparse.Namespace, parser: argparse.ArgumentParser) -> None:
+    raise exceptions.NotImplementedError()
--- experimenter/exceptions.py
+++ experimenter/exceptions.py
@@ -8,3 +8,7 @@ class ServerError(ExperimenterError):
 
 class InvalidStateError(ExperimenterError):
     pass
+
+
+class NotImplementedError(ExperimenterError):
+    pass
